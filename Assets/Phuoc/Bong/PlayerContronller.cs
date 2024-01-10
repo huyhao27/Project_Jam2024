e8f8731e19e22e70f43e9496de732e2e16f0dfd9
ChickMan <120817243+ChickMan2211@users.noreply.github.com>
2024-01-10 18:24:14 +0700

    di chuyen
    them di chuyen co ban

--- Assets/Phuoc/Bong/PlayerContronller.cs
+++ Assets/Phuoc/Bong/PlayerContronller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerContronller : MonoBehaviour
@@ -7,12 +8,33 @@ public class PlayerContronller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            transform.rotation = Quaternion.Euler(0, 0, transform.localEulerAngles.z - 90f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down;
+        }
+
     }
 }
